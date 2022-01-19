cd50b615722a895eb06f050957f7e58483f5b8d9
jacksomm <jack.somm1@gmail.com>
2022-01-19 16:57:58 -0700

    Retrieve new text on keypress, updated text_to_copy to stringvar

--- game.py
+++ game.py
@@ -8,15 +8,21 @@ class Game:
     self.root = Tk()
     self.running = False
     self.game_over = False
-    self.text_to_copy = ''
+
+    self.res = requests.get('https://random-word-api.herokuapp.com/word?number=10')
+    self.text_to_copy = StringVar()
+    self.text_to_copy.set(' '.join(self.res.json()))
+
+
     self.counter = -1
+
     self.minutes = StringVar() 
     self.seconds = StringVar()
     self.minutes.set("00")
     self.seconds.set("00")
 
-    self.instructions_text = "Please enter the amount of time you wish to test for. The timer will start on first key press."
-    self.instructions = Label(self.root, text=self.instructions_text, font=("Arial", 12))
+    self.instructions_text = "Please enter the amount of time you wish to test for. To start press tab inside the typing area. To retrieve new words press enter."
+    self.instructions = Label(self.root, text=self.instructions_text, font=("Arial", 11))
 
     self.minutes_label = Label(self.root, text="Minutes", font=("Arial", 18))
     self.seconds_label = Label(self.root, text="Seconds", font=("Arial", 18))
@@ -24,14 +30,13 @@ class Game:
     self.seconds_entry = Entry(self.root, width=3, font=("Arial", 18), textvariable=self.seconds)
 
     # Retreived paragraph to copy
-    self.text = Text(self.root, height=10, width=95)
-    self.text.insert(INSERT, self.text_to_copy)
-    self.text.config(state=DISABLED)
+    self.text = Label(self.root, height=4, width=30, textvariable=self.text_to_copy)
 
     # area to type in
-    self.type_area = Text(self.root, width=93, height=10)
+    self.type_area = Text(self.root, width=30, height=4, wrap=WORD)
     self.type_area.focus()
-    self.type_area.bind("<Key>", self.start)
+    self.type_area.bind("<Tab>", self.start)
+    self.type_area.bind("<Return>", self.get_new_words)
 
     self.quit_btn = ttk.Button(self.root, text="Quit", command=self.root.destroy)
 
@@ -65,15 +70,18 @@ class Game:
 
   def start(self, event):
     if not self.running:
-      self.counter = int(self.minutes.get())*60 + int(self.seconds.get())
       self.running = True
+      self.counter = int(self.minutes.get())*60 + int(self.seconds.get())
       self.count(self.counter)
 
   def pause(self):
     if self.running:
       self.root.after_cancel(self.after_loop)
       self.running = False
-    
 
+  def get_new_words(self, event):
+    if self.running:
+      self.res = requests.get('https://random-word-api.herokuapp.com/word?number=10')
+      self.text_to_copy.set(' '.join(self.res.json()))
 
 Game()
